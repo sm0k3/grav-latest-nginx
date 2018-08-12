607db6889efd8dc90895c56a5cc3684b7a563a11
Theros <Sm0k30X@gmail.com>
2018-08-12 23:04:43 +0100

    fix for new layout

--- setup/filesetup.sh
+++ setup/filesetup.sh
@@ -2,7 +2,5 @@
 cd /tmp
 git clone -b master https://github.com/getgrav/grav.git
 mv ./grav/user /srv/grav-data/user
-chown -R www-data:www-data /srv/grav-data/*
 mv ./grav /var/www/html
 ln -s /srv/grav-data/user /var/www/html/user
-chown -R www-data:www-data /var/www/*
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd /var/www/grav
+cd /var/www/html
 COMPOSER_ALLOW_SUPERUSER=1 php composer install --no-dev -o
 bin/grav install
 bin/gpm install admin
